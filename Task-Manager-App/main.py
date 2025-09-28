ac01ef1e87e8915b264fc96ccbe6b6d891ffeae3
Sambhav Kaushik <sambhavksk@gmail.com>
2025-09-28 23:18:02 +0530

    Starting pdf generator tool

--- Task-Manager-App/main.py
+++ Task-Manager-App/main.py
@@ -1,1 +1,10 @@
-print("Enter to do")
+user_text = input("Enter to do")
+print(user_text)
+user_text = int(user_text)
+nums = []
+for i in range(user_text):
+    val = int(input("Enter a number"))
+    nums.append(val)
+
+print(nums)
+print(type(nums))
---
+++ main.py
@@ -0,0 +1,1 @@
+print("This is a pdf generator tool project")
