ac9455ec012dea0e394e64585285931f430548b6
Sambhav Kaushik <sambhavksk@gmail.com>
2025-10-07 13:28:07 +0530

    Update of task manager app

--- Task-Manager-App/main.py
+++ Task-Manager-App/main.py
@@ -2,18 +2,31 @@ todos = []
 
 
 while True:
-    user_action = input("Enter add, show or exit: ")
+    user_action = input("Enter add, show,edit, complete or exit:")
     match user_action:
         case 'add':
-            todo = input("Enter a todo ")
+            todo = input("Enter a todo:")
             todos.append(todo)
         case 'show':
-            print(todos)
+            for i in range(0, len(todos)):
+                print(f"{i+1}. {todos[i]}")
+            print()
+        case 'edit':
+            number = int(input("Enter S.no of the todo to edit:"))
+            number -= 1
+            new_todo = input("Please enter a new todo:")
+            todos[number] = new_todo
+            print("Your list has been updated! ")
         case "exit":
             break
+        case "complete":
+            num = int(input("Which index is completed:"))
+            # num -= 1
+            # # todos.remove(todos[num])
+            todos.pop(num-1)
+            print("Your task is marked complete")
+        case blah_blah:
+            print("Please enter a valid command")
 
 
-print("bye")
-    
-
-
+print("Bye bye")
