aa0653dfdba9f0beec53c359139d4aff51afc47e
Sambhav Kaushik <sambhavksk@gmail.com>
2025-10-13 17:11:48 +0530

    Updated the web version

--- Task-Manager-App/Web-Version/main.py
+++ Task-Manager-App/Web-Version/main.py
@@ -1,5 +1,35 @@
-print("Hello world")
-print("HIIII")
-print("HOlaaa")
-print("NI haoo")
-print("Namaste")
+i = int(0)
+while True:
+    print(i)
+    i += 1
+    if i == 10:
+        break
+
+if i == 10:
+    while True:
+        print(i)
+        i += 1
+        if i ==  20:
+            break
+
+if i == 20:
+    while True:
+        print(i)
+        i += 1
+        if i == 30:
+            break
+
+if i == 30:
+    while True:
+        print(i)
+        i += 1
+        if i == 40:
+            break
+    
+
+while True:
+    print("Hello world")
+    i += 1
+    if i == 100:
+        break
+
